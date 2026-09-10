50f6257042e7972ebc7fda76bb3418673c8663c6
Olaleye Praise <65808506+Mpraizcool@users.noreply.github.com>
2026-09-10 15:43:22 +0100

    Add files via upload

---
+++ user.php
@@ -0,0 +1,11 @@
+<?php
+
+$sql = "CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin','student') DEFAULT 'student',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)";
+?>
