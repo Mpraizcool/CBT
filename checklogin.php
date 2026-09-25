f2633446b07bf5ba38cc6afb366d4c1010e71b0c
Olaleye Praise <65808506+Mpraizcool@users.noreply.github.com>
2026-09-25 06:19:00 +0100

    Refactor check_login function and add HTML structure

--- checklogin.php
+++ checklogin.php
@@ -11,3 +11,14 @@ if(strlen($_SESSION['login'])==0)
 	}
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
